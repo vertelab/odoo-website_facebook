d416a9d4b93ca16cb73216ed77da6513d06649bf
christelle wehbe <wehbechristelle@gmail.com>
2015-03-13 12:00:06 +0100

    info view fixed -- bank account added

--- website_facebook/website_facebook.py
+++ website_facebook/website_facebook.py
@@ -40,7 +40,7 @@ class website_facebook(http.Controller):
         return request.render('website_facebook.fb_page', ctx)   
         
     @http.route(['/fb/about','/<model("res.company"):company>/'], type='http', auth="public", website=True)
-    def facebook_about(self, user=False, company=False, **post):
+    def facebook_about(self, user=False, company=False,  **post):
         cr, uid, context, pool = request.cr, request.uid, request.context, request.registry
            
         ctx = {
