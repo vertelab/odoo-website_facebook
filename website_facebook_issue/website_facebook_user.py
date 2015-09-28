12ad75c45c5dc5d12b6dc8d551572f86c69a4457
christelle wehbe <wehbechristelle@gmail.com>
2015-09-28 09:34:28 +0200

    new files added

---
+++ website_facebook_issue/website_facebook_user.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+# OpenERP, Open Source Management Solution, third party addon
+# Copyright (C) 2004-2015 Vertel AB (<http://vertel.se>).
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+#~ from openerp import models, fields, api, _
+#~ from openerp.exceptions import except_orm, Warning, RedirectWarning
+#~ 
+#~ class facebook_user(models.Model):
+    #~ 
+    #~ created = fields.Datetime(default=fields.Datetime.now)
+    #~ updated = fields.Datetime(default=fields.Datetime.now)
+    #~ name = fields.Char(required=True)
+    #~ profile_url = fields.Char(required=True)
+    #~ access_token = fields.Char(required=True)         
